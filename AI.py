1a865fc01b1025c84b68141704fb96b9373c1329
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2025-11-15 21:00:08 +0700

    learning langchain

---
+++ AI.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+from langchain.chat_models import init_chat_model
+from langchain.agents import create_agent
+
+load_dotenv()
+# os.environ('GOOGLE_API_KEY') = os.getenv('GOOGLE_API_KEY')
+llm = init_chat_model(
+    api_key=os.getenv('GOOGLE_API_KEY'),
+    model="google_genai:gemini-2.5-flash-lite"
+    )
+
+def Ai(input: str):
+    print("mulai agent")
+    agent = create_agent(
+        model=llm
+    )
+    result = agent.invoke({
+        "messages":[{
+            "role":'user', "content":input
+        }]
+    })
+    return result
+
+
+if __name__ == "__main__":
+    perintah = input('Masukan perintah:')
+    hasil = Ai(perintah)
+    for h in hasil['messages']:
+        print(h.content)
